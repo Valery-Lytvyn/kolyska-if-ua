838bd833cb0ef3cacd040e9d863bf5fb99488b16
Valery Lytvyn <lytvynvalery@gmail.com>
2025-02-19 20:04:25 +0200

    After change API_URL

--- lib/fetchCatalogData.ts
+++ lib/fetchCatalogData.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const fetchCatalogData = async () => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"; // Fallback for local dev
+  const baseUrl =
+    process.env.NEXT_PUBLIC_API_URL || "https://kolyska-if-ua.vercel.app/"; // Fallback for local dev
 
   const response = await axios.get(`${baseUrl}/api/convertXmlToJson`, {
     responseType: "json",
