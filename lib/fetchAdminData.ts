f0671af37c7c23c5723e70d3d86eb0bb13a918ca
Valery Lytvyn <lytvynvalery@gmail.com>
2025-02-19 20:19:05 +0200

    After change API_URL

--- lib/fetchAdminData.ts
+++ lib/fetchAdminData.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchAdminData = async () => {
   const baseUrl =
-    process.env.NEXT_PUBLIC_API_URL || "https://kolyska-if-ua.vercel.app/"; // Fallback for local dev
+    process.env.NEXT_PUBLIC_API_URL || "https://kolyska-if-ua.vercel.app"; // Fallback for local dev
 
   const response = await axios.get(`${baseUrl}/api/adminData`, {
     responseType: "json",
--- lib/fetchCatalogData.ts
+++ lib/fetchCatalogData.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchCatalogData = async () => {
   const baseUrl =
-    process.env.NEXT_PUBLIC_API_URL || "https://kolyska-if-ua.vercel.app/"; // Fallback for local dev
+    process.env.NEXT_PUBLIC_API_URL || "https://kolyska-if-ua.vercel.app"; // Fallback for local dev
 
   const response = await axios.get(`${baseUrl}/api/convertXmlToJson`, {
     responseType: "json",
