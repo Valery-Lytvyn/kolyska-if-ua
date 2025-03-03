4d6565098050006b5b7b6a2debbfc034d3dee944
Valery Lytvyn <lytvynvalery@gmail.com>
2025-03-03 15:27:21 +0200

    Changed nextConfig fle

--- next.config.ts
+++ next.config.ts
@@ -20,16 +20,17 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-              default-src 'self';
-              script-src 'strict-dynamic' 'nonce-{random-nonce}' 'unsafe-eval' https://maps.googleapis.com https://www.google-analytics.com https://www.google.com https://www.gstatic.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.google.com https://www.gstatic.com;
-              font-src 'self' data: https://fonts.gstatic.com;
-              img-src 'self' data: https://babytilly.s3.amazonaws.com https://lh3.googleusercontent.com;
-              connect-src 'self' https://carrellobaby.com https://maps.googleapis.com https://maps.gstatic.com https://www.google-analytics.com https://*.mongodb.net;
-              frame-src 'self' https://www.google.com https://www.google.com/recaptcha/;
-              object-src 'none';
-              upgrade-insecure-requests;
-            `
+            default-src 'self';
+            script-src 'self' 'strict-dynamic' 'nonce-{random-nonce}' https://maps.googleapis.com https://www.google.com https://www.gstatic.com;
+            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.google.com https://www.gstatic.com;
+            font-src 'self' data: https://fonts.gstatic.com;
+            img-src 'self' data: https://babytilly.s3.amazonaws.com https://lh3.googleusercontent.com;
+            connect-src 'self' https://carrellobaby.com https://maps.googleapis.com https://maps.gstatic.com https://www.google-analytics.com https://*.mongodb.net;
+            frame-src 'self' https://www.google.com https://www.google.com/recaptcha/;
+            object-src 'none';
+            require-trusted-types-for 'script';
+            upgrade-insecure-requests;
+          `
               .replace(/\s{2,}/g, " ")
               .trim(),
           },
