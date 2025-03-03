5cdcc2b88895c621d6c54fc8aab624dfaf4185ac
Valery Lytvyn <lytvynvalery@gmail.com>
2025-03-03 16:39:10 +0200

    Changed nextConfig file

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,110 @@
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "babytilly.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
-  },
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "babytilly.s3.amazonaws.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com",
+//       },
+//     ],
+//   },
 
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: createSecureHeaders({
+//           contentSecurityPolicy: {
+//             directives: {
+//               defaultSrc: ["'self'"],
+//               scriptSrc: [
+//                 "'self'",
+//                 "'unsafe-inline'", // Додайте це, якщо ви використовуєте вбудовані скрипти
+//                 "https://www.google.com", // Для Google Maps
+//                 "https://www.gstatic.com", // Для Google reCAPTCHA
+//               ],
+//               styleSrc: [
+//                 "'self'",
+//                 "'unsafe-inline'", // Додайте це, якщо ви використовуєте вбудовані стилі
+//                 "https://fonts.googleapis.com", // Для Google Fonts
+//               ],
+//               imgSrc: [
+//                 "'self'",
+//                 "data:",
+//                 "https://babytilly.s3.amazonaws.com", // Для зображень з S3
+//                 "https://www.google.com", // Для Google Maps
+//               ],
+//               fontSrc: [
+//                 "'self'",
+//                 "https://fonts.gstatic.com", // Для Google Fonts
+//               ],
+//               frameSrc: [
+//                 "'self'",
+//                 "https://www.google.com", // Для Google Maps
+//               ],
+//               connectSrc: [
+//                 "'self'",
+//                 "https://carrellobaby.com", // Для axios запитів
+//                 "https://your-mongodb-uri.com", // Для підключення до MongoDB
+//               ],
+//             },
+//           },
+//         }),
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+import { createSecureHeaders } from "next-secure-headers";
+
+module.exports = {
   async headers() {
     return [
       {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-            default-src 'self';
-            script-src 'self' 'strict-dynamic' 'nonce-{random-nonce}' https://maps.googleapis.com https://www.google.com https://www.gstatic.com;
-            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.google.com https://www.gstatic.com;
-            font-src 'self' data: https://fonts.gstatic.com;
-            img-src 'self' data: https://babytilly.s3.amazonaws.com https://lh3.googleusercontent.com;
-            connect-src 'self' https://carrellobaby.com https://maps.googleapis.com https://maps.gstatic.com https://www.google-analytics.com https://*.mongodb.net;
-            frame-src 'self' https://www.google.com https://www.google.com/recaptcha/;
-            object-src 'none';
-            require-trusted-types-for 'script';
-            upgrade-insecure-requests;
-          `
-              .replace(/\s{2,}/g, " ")
-              .trim(),
+        source: "/(.*)", // Застосовується до всіх шляхів
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'"],
+              scriptSrc: [
+                "'self'",
+                "'unsafe-inline'", // Для вбудованих скриптів
+                "https://www.google.com", // Для Google Maps
+                "https://www.gstatic.com", // Для Google reCAPTCHA
+              ],
+              styleSrc: [
+                "'self'",
+                "'unsafe-inline'", // Для вбудованих стилів
+                "https://fonts.googleapis.com", // Для Google Fonts
+              ],
+              imgSrc: [
+                "'self'",
+                "data:",
+                "https://babytilly.s3.amazonaws.com", // Для зображень з S3
+                "https://www.google.com", // Для Google Maps
+              ],
+              fontSrc: [
+                "'self'",
+                "https://fonts.gstatic.com", // Для Google Fonts
+              ],
+              frameSrc: [
+                "'self'",
+                "https://www.google.com", // Для Google Maps
+              ],
+              connectSrc: [
+                "'self'",
+                "https://carrellobaby.com", // Для axios запитів
+                "https://your-mongodb-uri.com", // Для підключення до MongoDB
+              ],
+            },
           },
-        ],
+        }),
       },
     ];
   },
 };
-
-export default nextConfig;
